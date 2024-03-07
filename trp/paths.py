9f631cdb86b335cb82de766cccd78130c4321a4a
lgbouma <bouma.luke@gmail.com>
2024-03-07 10:01:20 -0800

    add cachedir

--- trp/paths.py
+++ trp/paths.py
@@ -7,7 +7,11 @@ DATADIR = os.path.join(__path__[0], 'data')
 # archive (backed-up) results here
 RESULTSDIR = os.path.join(os.path.dirname(__path__[0]), 'results')
 
-for l in [DATADIR, RESULTSDIR]:
+# cache for temporary files
+CACHEDIR = join(os.path.expanduser('~'), '.trp_cache')
+if not os.path.exists(CACHEDIR): os.mkdir(CACHEDIR)
+
+for l in [DATADIR, RESULTSDIR, CACHEDIR]:
     if not os.path.exists(l):
         print(f"Making {l}")
         os.mkdir(l)
