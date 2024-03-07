dd98af4714b5143be25740edf147f4ef04a6e7d2
lgbouma <bouma.luke@gmail.com>
2024-03-07 15:09:57 -0600

    stage HTCondor wrappers

---
+++ drivers/run_pipeline.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Unpack the envvironment (with the trp package), and activate it
+tar -xzf py38.tar.gz
+export PYTHONPATH=$PWD/py38
+
+# Run the main Python script; called as run_trp.py <sample_id>.
+python3 run_trp.py $1
+
+# Move and tarball output (log files) to top level directory for condor return.
+mkdir $_CONDOR_SCRATCH_DIR/joboutput_$1
+mv /srv/.trp_cache/rotperiod_finding/*$1*/*log $_CONDOR_SCRATCH_DIR/joboutput_$1/.
+tar czf joboutput_$1.tar.gz $_CONDOR_SCRATCH_DIR/joboutput_$1
