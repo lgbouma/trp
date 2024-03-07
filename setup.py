bdc2b9a7a14aad703fea2cc1b260a2fe2ce2037d
lgbouma <bouma.luke@gmail.com>
2024-03-06 17:44:36 -0800

    clean setup

--- setup.py
+++ setup.py
@@ -32,13 +32,6 @@ def readme():
     with open('README.md') as f:
         return f.read()
 
-INSTALL_REQUIRES = [
-    'numpy>=1.4.0',
-    'scipy',
-    'astropy>=1.3',
-    'matplotlib',
-]
-
 ###############
 ## RUN SETUP ##
 ###############
@@ -66,7 +59,6 @@ setup(
     packages=[
         'trp',
     ],
-    install_requires=INSTALL_REQUIRES,
     tests_require=['pytest==3.8.2',],
     cmdclass={'test':PyTest},
     include_package_data=True,
