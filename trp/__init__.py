a9941c3911aab1f155fcd9faf996a8933d7d6102
lgbouma <bouma.luke@gmail.com>
2024-03-07 10:01:10 -0800

    add standard logging

---
+++ trp/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.0.0"
+
+# the basic logging styles common to all package modules
+log_sub = '{'
+log_fmt = '[{levelname:1.1} {asctime} {module}:{lineno}] {message}'
+log_date_fmt = '%y%m%d %H:%M:%S'
