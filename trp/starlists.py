752342457355f70c8d7069c842bc74d9669751f9
lgbouma <bouma.luke@gmail.com>
2024-03-11 18:19:41 -0700

    starlists: OSG chunking

--- trp/starlists.py
+++ trp/starlists.py
@@ -36,6 +36,8 @@ from os.path import join
 from glob import glob
 import numpy as np, pandas as pd
 
+from trp.paths import DATADIR
+
 #############
 
 def get_ticids(sample_id, lcpipeline):
@@ -65,14 +67,29 @@ def get_ticids(sample_id, lcpipeline):
         N_stars_to_search = len(ticids)
         N_lcs_to_search = -1
 
-    elif 'pc' in sample_id and 'to' in sample_id and lcpipeline=='qlp':
+    # CONSTRUCTION OF THE JOBS
+    elif (
+        'pc' in sample_id and 'to' in sample_id
+        and lcpipeline=='qlp'
+        and 'job' not in sample_id
+    ):
 
         lower = int(sample_id.split("to")[0])
         upper = int(sample_id.split("to")[1].split("pc")[0])
 
+        if lower == 0:
+            lower = 1
+
         assert upper <= 500 # parsecs
 
-        df = pd.read_csv(join(QLPDIR, "QLP_s1s55_X_GDR2_parallax_gt_2.csv"))
+        # As described:
+        # Generated on wh1 at /ar1/TESS/QLP ;  I downloaded all S1-S55 QLP
+        # light curves.  Got the TICID's.  Crossmatched against Gaia DR2 to get
+        # the parallaxes.  The TICID's and parallaxes for plx>2
+        # (distance<500pc) are inside this file.
+        df = pd.read_csv(
+            join(DATADIR, "QLP_s1s55_X_GDR2_parallax_gt_2_ticid_plx.tar.gz")
+        )
 
         sel = (
             (df["parallax"] <= 1e3*(1/lower))
@@ -84,33 +101,37 @@ def get_ticids(sample_id, lcpipeline):
         ticids = np.unique(list(sdf["ticid"].astype(str)))
 
         N_stars_to_search = len(ticids)
-        N_lcs_to_search = len(sdf) # this is incorrect; in 2023, it's ~3x N_stars_to_search
+        N_lcs_to_search = len(sdf) # this is incorrect; in 2024, it's ~3x N_stars_to_search
 
-    # e.g., 30to50pc_mkdwarf, 50to60pc_mkdwarf, etc.
-    elif 'pc_mkdwarf' in sample_id and 'to' in sample_id and lcpipeline=='spoc2min':
+    elif (
+        'pc' in sample_id and 'to' in sample_id
+        and lcpipeline=='qlp'
+        and 'job' in sample_id
+    ):
 
+        # Run on OSG!
         lower = int(sample_id.split("to")[0])
         upper = int(sample_id.split("to")[1].split("pc")[0])
 
-        df = pd.read_csv(join(SPOCDIR, "gaia_X_spoc2min_merge.csv"))
+        if lower == 0:
+            lower = 1
 
-        sel = (
-            (df["M_G"] > 4)
-            &
-            (df["bp_rp"] > 1.5)
-            &
-            (df["TESSMAG"] < 16)
-            &
-            (df["parallax"] <= 1e3*(1/lower))
-            &
-            (df["parallax"] > 1e3*(1/upper))
-        )
+        assert upper <= 500 # parsecs
 
-        sdf = df[sel]
-        ticids = np.unique(list(sdf["TICID"].astype(str)))
+        # this file is transferred via the HTCondor submit script
+        csvpath = f"./{sample_id}.csv"
+        LOGINFO(f"Attempting to get TICIDs from {csvpath}...")
+        df = pd.read_csv(csvpath)
+
+        ticids = np.unique(list(df["ticid"].astype(str)))
 
         N_stars_to_search = len(ticids)
-        N_lcs_to_search = len(sdf)
+        N_lcs_to_search = 3*N_stars_to_search # this is very approximate
+
+
+    # e.g., 30to50pc_mkdwarf, 50to60pc_mkdwarf, etc.
+    elif 'pc_mkdwarf' in sample_id and 'to' in sample_id and lcpipeline=='spoc2min':
+        raise NotImplementedError
 
     else:
         raise NotImplementedError
