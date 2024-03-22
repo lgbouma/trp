6cc9ae73b346215c7a2a9478a8aec2702045b692
lgbouma <bouma.luke@gmail.com>
2024-03-22 15:57:36 -0500

    exec_pipeline: dont return lc

--- drivers/exec_pipeline.sh
+++ drivers/exec_pipeline.sh
@@ -7,10 +7,11 @@ export PYTHONPATH=$PWD/py38
 # Run the main Python script; called as run_trp.py <sample_id>.
 python3 run_trp.py $1
 
-# Move and tarball output (log and pkl files, plus the MAST download) to top
-# level directory for condor return.
+# Move and tarball output (log and pkl files) to top level directory for condor
+# return.  Could also move MAST downloads, but don't!
 mkdir $_CONDOR_SCRATCH_DIR/joboutput_$1
 mv /srv/.trp_cache/rotperiod_finding/*$1*/*log $_CONDOR_SCRATCH_DIR/joboutput_$1/.
 mv /srv/.trp_cache/rotperiod_finding/*$1*/*pkl $_CONDOR_SCRATCH_DIR/joboutput_$1/.
-mv /srv/.trp_cache/rotperiod_finding/*$1*/mastDownload $_CONDOR_SCRATCH_DIR/joboutput_$1/.
+#mv /srv/.trp_cache/rotperiod_finding/*$1*/mastDownload $_CONDOR_SCRATCH_DIR/joboutput_$1/.
+
 tar czf joboutput_$1.tar.gz $_CONDOR_SCRATCH_DIR/joboutput_$1
