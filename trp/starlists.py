c3f367681128d4ffb47781b24459bf39d2391e4f
lgbouma <bouma.luke@gmail.com>
2024-03-25 21:47:06 -0700

    debug: better test star

--- trp/starlists.py
+++ trp/starlists.py
@@ -45,7 +45,8 @@ def get_ticids(sample_id, lcpipeline):
 
     if sample_id == 'debug':
         ticids = [
-            "402980664"
+            "15756231"
+            #"402980664"
         ]
 
         N_stars_to_search = len(ticids)
