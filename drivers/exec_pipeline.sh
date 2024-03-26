862494ddaf0329755fa0b7d89a49689674c60bd3
lgbouma <bouma.luke@gmail.com>
2024-03-26 11:31:27 -0500

    submit scripts: better docs

--- drivers/exec_pipeline.sh
+++ drivers/exec_pipeline.sh
@@ -7,11 +7,22 @@ export PYTHONPATH=$PWD/py38
 # Run the main Python script; called as run_trp.py <sample_id>.
 python3 run_trp.py $1
 
-# Move and tarball output (log and pkl files) to top level directory for condor
-# return.  Could also move MAST downloads, but don't!
+# Move and tarball output (log  files, pkl files, anything downloaded from
+# MAST) to top level directory for condor to return.  Everything will be tar'd.
+save_log=true
+save_pkl=false
+save_mastDownload=false
+
 mkdir $_CONDOR_SCRATCH_DIR/joboutput_$1
-mv /srv/.trp_cache/rotperiod_finding/*$1*/*log $_CONDOR_SCRATCH_DIR/joboutput_$1/.
-mv /srv/.trp_cache/rotperiod_finding/*$1*/*pkl $_CONDOR_SCRATCH_DIR/joboutput_$1/.
-#mv /srv/.trp_cache/rotperiod_finding/*$1*/mastDownload $_CONDOR_SCRATCH_DIR/joboutput_$1/.
+
+if [ "$save_log" = true ]; then
+    mv /srv/.trp_cache/rotperiod_finding/*$1*/*log $_CONDOR_SCRATCH_DIR/joboutput_$1/.
+fi
+if [ "$save_pkl" = true ]; then
+    mv /srv/.trp_cache/rotperiod_finding/*$1*/*pkl $_CONDOR_SCRATCH_DIR/joboutput_$1/.
+fi
+if [ "$move_mastDownload" = true ]; then
+    mv /srv/.trp_cache/rotperiod_finding/*$1*/mastDownload $_CONDOR_SCRATCH_DIR/joboutput_$1/.
+fi
 
 tar czf joboutput_$1.tar.gz $_CONDOR_SCRATCH_DIR/joboutput_$1
