9ec33ca83f648d23b656958a67f5ab3cc86ed915
lgbouma <bouma.luke@gmail.com>
2024-10-10 09:34:06 -0700

    run_trp: argparse the driver

--- drivers/run_trp.py
+++ drivers/run_trp.py
@@ -1,15 +1,50 @@
 """
-Usage: `python run_trp.py <sample_id>`
+Usage: `python run_trp.py <sample_id> [--maskknowntransits]`
 See docstring for trp_pipeline.run_trp for verbose explanation.
 
-(Example: `python run_trp.py debug`)
+Example usage:
+`python run_trp.py debug`
+`python run_trp.py example_hjlist.csv --maskknowntransits --writevetplot`
 """
 import sys
+import argparse
 from trp.trp_pipeline import run_trp
 
 def main():
-    sample_id = sys.argv[1]
-    run_trp(sample_id)
+
+    parser = argparse.ArgumentParser(description="Run TRP pipeline")
+
+    parser.add_argument(
+        "sample_id",
+        help="Sample ID to process (see trp_pipeline.py)"
+    )
+
+    parser.add_argument(
+        "--maskknowntransits",
+        action="store_true",
+        help="Search for known transits and mask them (default: False)"
+    )
+
+    parser.add_argument(
+        "--writevetplot",
+        action="store_true",
+        help="Make a plot showing result of Prot search (default:False)"
+    )
+
+    parser.add_argument(
+        "--forcerun",
+        action="store_true",
+        help="Overwrite pre-existing cached files (default:False)"
+    )
+
+    args = parser.parse_args()
+
+    run_trp(
+        args.sample_id,
+        mask_known_transits=args.maskknowntransits,
+        write_vetplot=args.writevetplot,
+        forcerun=args.forcerun
+    )
 
 if __name__ == "__main__":
     main()
