2654a394a48e6962173faa8b7a737ef74e5d04d8
lgbouma <bouma.luke@gmail.com>
2025-11-04 13:36:44 -0800

    lcprocessing: add time_bin_lightcurve

--- trp/lcprocessing.py
+++ trp/lcprocessing.py
@@ -2,6 +2,7 @@
 Contents:
     | rotation_periodsearch
     | calculate_lsp
+    | time_bin_lightcurve
 """
 
 #######################################
@@ -399,3 +400,27 @@ def calculate_lsp(times, fluxes, starid=None, outdir=None, nyquist_factor=2,
             LOGINFO(f'Made {pklpath}')
 
     return results
+
+
+def time_bin_lightcurve(time, flux, binsize):
+    """Bin the light curve in time with a fixed binsize.
+    Returns arrays of binned time (bin centers) and mean flux in each bin."""
+
+    t_min, t_max = np.nanmin(time), np.nanmax(time)
+    # Define bin edges from t_min to t_max
+    edges = np.arange(t_min, t_max + binsize, binsize)
+    # Digitize assigns each time to a bin index
+    bin_idx = np.digitize(time, edges) - 1
+
+    binned_time = []
+    binned_flux = []
+    # Iterate over all bins
+    for i in range(len(edges) - 1):
+        mask = bin_idx == i
+        if not np.any(mask):
+            continue
+        # Compute mean time and flux in the bin
+        binned_time.append(np.nanmean(time[mask]))
+        binned_flux.append(np.nanmean(flux[mask]))
+
+    return np.array(binned_time), np.array(binned_flux)
